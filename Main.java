6dee3b8cd0d0cfe97a82cbcbb0deee795e2cc653
mahekbhutiya <mahekbhutiya19@gmail.com>
2025-11-25 17:58:13 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java Programming!");
+        System.out.println("This is my first Java code.");
+    }
+}
+
