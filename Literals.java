b94ebae1b6271e4ec9489f85189b48ed3dbb6f2f
mahekbhutiya <mahekbhutiya19@gmail.com>
2025-11-25 17:37:58 +0530

    Add files via upload

---
+++ Literals.java
@@ -0,0 +1,28 @@
+public class Literals {
+        public static void main(String[] args) {
+        
+        // Integer literal
+        int intLiteral = 100;
+
+        // Floating-point literal
+        double floatLiteral = 45.67;
+
+        // Character literal
+        char charLiteral = 'J';
+
+        // String literal
+        String stringLiteral = "Java Programming";
+
+        // Boolean literal
+        boolean booleanLiteral = true;
+
+        // Printing all literals
+        System.out.println(intLiteral);
+        System.out.println(floatLiteral);
+        System.out.println(charLiteral);
+        System.out.println(stringLiteral);
+        System.out.println(booleanLiteral);
+    }
+}
+
+
